83d837e5317d4e8aa05a98f5d2f0d8d05ae7fdba
Diego De Souza <a.t.dieps17@yahoo.com>
2014-12-22 13:39:56 -0500

    email has already been registered test passing

--- book_reviews_in_space/app/helpers/application_helper.rb
+++ book_reviews_in_space/app/helpers/application_helper.rb
@@ -1,2 +1,5 @@
 module ApplicationHelper
+  def devise_error_messages!
+    resource.errors.full_messages.map { |msg| content_tag(:li, msg) }.join
+  end
 end
--- book_reviews_in_space/spec/features/user_signs_up_spec.rb
+++ book_reviews_in_space/spec/features/user_signs_up_spec.rb
@@ -15,15 +15,39 @@ require 'rails_helper'
   - [ ] If my email is not formatted correctly, I am given an error message.
 ) do
 
-  it "user provides valid information" do
+  it "provides valid information" do
+
     visit '/'
+
     click_link("Sign up", match: :first)
     fill_in "Name", with: "Diego De Souza"
     fill_in "Email", with: "example@example.com"
     fill_in "Password", with: "password"
     fill_in "Password confirmation", with: "password"
     click_button "Sign up"
-        expect(page).to have_content "Welcome! You have signed up successfully."
+
+    expect(page).to have_content "Welcome! You have signed up successfully."
+  end
+
+  it "email has already been registered" do
+
+    existing_user = User.create(
+      name: "Diego De Souza",
+      email: "example@example.com",
+      password: "password"
+    )
+
+    visit '/'
+
+    click_link("Sign up", match: :first)
+    fill_in "Name", with: existing_user.name
+    fill_in "Email", with: existing_user.email
+    fill_in "Password", with: existing_user.password
+    fill_in "Password confirmation", with: existing_user.password
+    click_button "Sign up"
     save_and_open_page
+
+    expect(page).to have_content "Email has already been taken"
+
   end
 end
