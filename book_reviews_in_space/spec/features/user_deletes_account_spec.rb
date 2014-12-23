3731f7d5cf324829fb796c263b073f0dc545e2d4
Diego De Souza <a.t.dieps17@yahoo.com>
2014-12-23 00:13:16 -0500

    message being prompt

---
+++ book_reviews_in_space/spec/features/user_deletes_account_spec.rb
@@ -0,0 +1,30 @@
+require 'rails_helper'
+
+  feature "User deletes account", %q(
+  As an authenticated user
+  I want to delete my account
+  So that my information is no longer retained by the app
+
+  - [] In my home page I can navigate to edit profile and chose to delete my account
+  - [] When clicking delete my account i should see a message "are you sure?"
+  - [] if i click delete my account it should delete it from my database
+  ) do
+
+  scenario "user deletes his account" do
+    user = create(:user)
+    current_user#as logged in
+
+    visit root_path
+
+    click_on "Edit profile"
+    click_on "Cancel my account"
+
+    redirect_to root_path
+    expect(page).to have_content "Bye! Your account has been successfully cancelled. We hope to see you again soon."
+
+  scenario "user sees a message after clicking delete my account"
+
+
+
+  end
+end
