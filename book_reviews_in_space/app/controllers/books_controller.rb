18ba5e7bb6a5e1f2432ac568bf11fc8de9c9338c
Diego De Souza <a.t.dieps17@yahoo.com>
2014-12-22 23:17:16 -0500

    user logs out

--- book_reviews_in_space/app/controllers/books_controller.rb
+++ book_reviews_in_space/app/controllers/books_controller.rb
@@ -1,4 +1,36 @@
 class BooksController < ApplicationController
   def index
+
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      log_in @user
+      flash[:success] = "Welcome to the books review!"
+      redirect_to @user
+    else
+      render 'new'
+    end
+  end
+
+  def destroy
+    destroy_user_session_path
+    redirect_to root_path
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password,
+    :password_confirmation)
   end
 end
---
+++ book_reviews_in_space/spec/features/user_deletes_accout_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+feature "User deletes account", %q(
+As an authenticated user
+I want to delete my account
+So that my information is no longer retained by the app
+
+- [] I can navigate to edit profile and chose to delete my account
+- [] if i click delete my account it should delete from my database
+- []
+) do
+
+  scenario "user deletes his account"
+
+  current_user#as logged in
+
+  visit root_path
+
+  click_on "Edit profile"
+  click_on "Cancel my account"
+
+  expect(page).to have_content "<some message here>"
+end
