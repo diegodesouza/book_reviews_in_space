aecc53067acc8a993eef4df067bde91e6257326a
Diego De Souza <a.t.dieps17@yahoo.com>
2014-12-23 14:12:58 -0500

    invalid entries errors displayed. tests for these errors passing.

--- book_reviews_in_space/app/models/book.rb
+++ book_reviews_in_space/app/models/book.rb
@@ -5,4 +5,5 @@ class Book < ActiveRecord::Base
   validates :title, presence: true
   validates :description, presence: true
   validates :url, presence: true
+  validates_format_of :url, :with => URI::regexp(%w(http https))
 end
--- book_reviews_in_space/spec/features/user_can_post_books_and_review_them_spec.rb
+++ book_reviews_in_space/spec/features/user_can_post_books_and_review_them_spec.rb
@@ -8,6 +8,7 @@ So that I can post books and review them
 - [] I can sign in
 - [] I can post a book
 - [] I can review my books
+- [] I am presented with errors if i fill out the form incorrectly
 ) do
 
   it "signs in and post a book for review" do
@@ -32,5 +33,108 @@ So that I can post books and review them
 
     expect(page).to have_content "Your have successfully added a new book."
 
+  end
+
+  it "gives me errors if leave title blank" do
+    user = create :user
+    password = generate :password
+    email = generate :email
+
+    visit root_path
+
+    click_link("Sign up", match: :first)
+    fill_in "Name", with: user.name
+    fill_in "Email", with: email
+    fill_in "Password", with: password
+    fill_in "Password confirmation", with: password
+    click_button "Sign up"
+
+    click_on "Add new book"
+
+    fill_in "Title", with: ""
+    fill_in "Description", with: "Try this"
+    fill_in "URL", with: "https//new.com"
+
+    click_on "Add book"
+
+
+    expect(page).to have_content "Title can't be blank"
+  end
+
+  it "gives me errors if leave description blank" do
+    user = create :user
+    password = generate :password
+    email = generate :email
+
+    visit root_path
+
+    click_link("Sign up", match: :first)
+    fill_in "Name", with: user.name
+    fill_in "Email", with: email
+    fill_in "Password", with: password
+    fill_in "Password confirmation", with: password
+    click_button "Sign up"
+
+    click_on "Add new book"
+
+    fill_in "Title", with: "Some Title"
+    fill_in "Description", with: ""
+    fill_in "URL", with: "https//new.com"
+
+    click_on "Add book"
+
+
+    expect(page).to have_content "Description can't be blank"
+  end
+
+  it "gives me errors if leave url blank" do
+    user = create :user
+    password = generate :password
+    email = generate :email
+
+    visit root_path
+
+    click_link("Sign up", match: :first)
+    fill_in "Name", with: user.name
+    fill_in "Email", with: email
+    fill_in "Password", with: password
+    fill_in "Password confirmation", with: password
+    click_button "Sign up"
+
+    click_on "Add new book"
+
+    fill_in "Title", with: "Some title"
+    fill_in "Description", with: "Try this"
+    fill_in "URL", with: ""
+
+    click_on "Add book"
+
+
+    expect(page).to have_content "Url can't be blank"
+  end
+  
+  it "gives me errors if url is invalid" do
+    user = create :user
+    password = generate :password
+    email = generate :email
+
+    visit root_path
+
+    click_link("Sign up", match: :first)
+    fill_in "Name", with: user.name
+    fill_in "Email", with: email
+    fill_in "Password", with: password
+    fill_in "Password confirmation", with: password
+    click_button "Sign up"
+
+    click_on "Add new book"
+
+    fill_in "Title", with: "Some title"
+    fill_in "Description", with: "Try this"
+    fill_in "URL", with: "www.new.com"
+
+    click_on "Add book"
+
+    expect(page).to have_content "Url is invalid"
   end
 end
